805abd1f54cc3266b05a9f71af53cf3d18b93c52
Zac-Mwangi <zackmwangi998@gmail.com>
2022-05-14 23:08:43 +0300

    checks if the questions have been answered or not

--- assests/js/results.js
+++ assests/js/results.js
@@ -1,2 +1,20 @@
 percentage = 0;
 remark = " ";
+
+function submitToJs() {
+
+    marks = 0;
+
+    // The checked property sets or returns the checked state of a checkbox.
+
+    // check inputs if selected
+
+    // question 1
+    if (document.getElementById('t1').checked ||
+        document.getElementById('af1').checked ||
+        document.getElementById('bf1').checked ||
+        document.getElementById('cf1').checked) {} else {
+        alert("Please answer Question 1 ");
+        return false;
+    }
+}
