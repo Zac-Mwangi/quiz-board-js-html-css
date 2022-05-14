a00aacfbb445b3733d1ce7f2cf13144aa29fcd8d
Zac-Mwangi <zackmwangi998@gmail.com>
2022-05-14 23:10:48 +0300

    find the percentage of all the correct questions

--- assests/js/results.js
+++ assests/js/results.js
@@ -1,5 +1,5 @@
-percentage = 0;
-remark = " ";
+per = 0;
+present = " ";
 
 function submitToJs() {
 
@@ -25,4 +25,83 @@ function submitToJs() {
         alert("Please answer Question 1 ");
         return false;
     }
+
+    // question2
+    if (document.getElementById('t2').checked ||
+        document.getElementById('af2').checked ||
+        document.getElementById('bf2').checked ||
+        document.getElementById('cf2').checked) {
+
+        var q2 = document.getElementById('t2').checked;
+
+        if (q2 == true) {
+            marks = marks + 1;
+        } else {
+            marks = marks + 0;
+        }
+
+    } else {
+        alert("Please answer question 2 ");
+        return false;
+    }
+
+    // question3
+    if (document.getElementById('t3').checked ||
+        document.getElementById('af3').checked ||
+        document.getElementById('bf3').checked ||
+        document.getElementById('cf3').checked) {
+
+        var q3 = document.getElementById('t3').checked;
+
+        if (q3 == true) {
+            marks = marks + 1;
+        } else {
+            marks = marks + 0;
+        }
+
+    } else {
+        alert("Please answer Question 3 ");
+        return false;
+    }
+
+    // question4
+    if (document.getElementById('t4').checked ||
+        document.getElementById('af4').checked ||
+        document.getElementById('bf4').checked ||
+        document.getElementById('cf4').checked) {
+
+        var q4 = document.getElementById('t4').checked;
+
+        if (q4 == true) {
+            marks = marks + 1;
+        } else {
+            marks = marks + 0;
+        }
+
+    } else {
+        alert("Please answer Question 4 ");
+        return false;
+    }
+
+    // question5
+    if (document.getElementById('t5').checked ||
+        document.getElementById('af5').checked ||
+        document.getElementById('bf5').checked ||
+        document.getElementById('cf5').checked) {
+
+        var q5 = document.getElementById('t5').checked;
+
+        if (q5 == true) {
+            marks = marks + 1;
+        } else {
+            marks = marks + 0;
+        }
+
+    } else {
+        alert("Please answer Question 5 ");
+        return false;
+    }
+
+    var per = (marks / 5) * 100;
+
 }
