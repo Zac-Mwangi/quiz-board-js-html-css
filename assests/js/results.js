758b602af9c0d13aecd62bafbfae5e42a5e7f801
Zac-Mwangi <zackmwangi998@gmail.com>
2022-05-14 23:10:05 +0300

    on js file result.js i check if the checked value is the true value and awards it a point

--- assests/js/results.js
+++ assests/js/results.js
@@ -13,7 +13,15 @@ function submitToJs() {
     if (document.getElementById('t1').checked ||
         document.getElementById('af1').checked ||
         document.getElementById('bf1').checked ||
-        document.getElementById('cf1').checked) {} else {
+        document.getElementById('cf1').checked) {
+
+        var q1 = document.getElementById('t1').checked;
+        if (q1 == true) {
+            marks = marks + 1;
+        } else {
+            marks = marks + 0;
+        }
+    } else {
         alert("Please answer Question 1 ");
         return false;
     }
