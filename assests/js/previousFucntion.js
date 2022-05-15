e920e2d6b5ae8f943d9298b36664733ab9684b3d
Zac-Mwangi <zackmwangi998@gmail.com>
2022-05-16 00:14:21 +0300

    add the file called previousFunctions.js and all the events needed

---
+++ assests/js/previousFucntion.js
@@ -0,0 +1,48 @@
+function previous1() {
+    alert("No Previous Question ! ! !")
+    return false;
+}
+
+function previous2() {
+    document.getElementsByClassName('bigContainer')[0].style.display = "block";
+    document.getElementsByClassName('bigContainer')[1].style.display = "none";
+    document.getElementsByClassName('bigContainer')[2].style.display = "none";
+    document.getElementsByClassName('bigContainer')[3].style.display = "none";
+    document.getElementsByClassName('bigContainer')[4].style.display = "none";
+
+    return false;
+
+}
+
+function previous3() {
+    document.getElementsByClassName('bigContainer')[0].style.display = "none";
+    document.getElementsByClassName('bigContainer')[1].style.display = "block";
+    document.getElementsByClassName('bigContainer')[2].style.display = "none";
+    document.getElementsByClassName('bigContainer')[3].style.display = "none";
+    document.getElementsByClassName('bigContainer')[4].style.display = "none";
+
+    return false;
+
+}
+
+function previous4() {
+    document.getElementsByClassName('bigContainer')[0].style.display = "none";
+    document.getElementsByClassName('bigContainer')[1].style.display = "none";
+    document.getElementsByClassName('bigContainer')[2].style.display = "block";
+    document.getElementsByClassName('bigContainer')[3].style.display = "none";
+    document.getElementsByClassName('bigContainer')[4].style.display = "none";
+
+    return false;
+
+}
+
+function previous5() {
+    document.getElementsByClassName('bigContainer')[0].style.display = "none";
+    document.getElementsByClassName('bigContainer')[1].style.display = "none";
+    document.getElementsByClassName('bigContainer')[2].style.display = "none";
+    document.getElementsByClassName('bigContainer')[3].style.display = "block";
+    document.getElementsByClassName('bigContainer')[4].style.display = "none";
+
+    return false;
+
+}
