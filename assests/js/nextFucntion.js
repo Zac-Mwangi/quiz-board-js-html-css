b3951915c1466c377db31da14402dabd3b93be24
Zac-Mwangi <zackmwangi998@gmail.com>
2022-05-15 22:41:15 +0300

    checks if each question has been answered

--- assests/js/nextFucntion.js
+++ assests/js/nextFucntion.js
@@ -1,44 +1,90 @@
-document.getElementsByClassName('question_container')[0].style.display = "block";
+document.getElementsByClassName('bigContainer')[0].style.display = "block";
 
 function next1() {
-    document.getElementsByClassName('question_container')[0].style.display = "none";
-    document.getElementsByClassName('question_container')[1].style.display = "block";
-    document.getElementsByClassName('question_container')[2].style.display = "none";
-    document.getElementsByClassName('question_container')[3].style.display = "none";
-    document.getElementsByClassName('question_container')[4].style.display = "none";
+
+    if (document.getElementById('t1').checked ||
+        document.getElementById('af1').checked ||
+        document.getElementById('bf1').checked ||
+        document.getElementById('cf1').checked) {
+
+    } else {
+        alert("Please answer Question 1 ");
+        return false;
+    }
+
+    document.getElementsByClassName('bigContainer')[0].style.display = "none";
+    document.getElementsByClassName('bigContainer')[1].style.display = "block";
+    document.getElementsByClassName('bigContainer')[2].style.display = "none";
+    document.getElementsByClassName('bigContainer')[3].style.display = "none";
+    document.getElementsByClassName('bigContainer')[4].style.display = "none";
 
     return false;
 }
 
 function next2() {
-    document.getElementsByClassName('question_container')[0].style.display = "none";
-    document.getElementsByClassName('question_container')[1].style.display = "none";
-    document.getElementsByClassName('question_container')[2].style.display = "block";
-    document.getElementsByClassName('question_container')[3].style.display = "none";
-    document.getElementsByClassName('question_container')[4].style.display = "none";
+
+    if (document.getElementById('t2').checked ||
+        document.getElementById('af2').checked ||
+        document.getElementById('bf2').checked ||
+        document.getElementById('cf2').checked) {
+
+    } else {
+        alert("Please answer question 2 ");
+        return false;
+    }
+
+    document.getElementsByClassName('bigContainer')[0].style.display = "none";
+    document.getElementsByClassName('bigContainer')[1].style.display = "none";
+    document.getElementsByClassName('bigContainer')[2].style.display = "block";
+    document.getElementsByClassName('bigContainer')[3].style.display = "none";
+    document.getElementsByClassName('bigContainer')[4].style.display = "none";
 
     return false;
 
 }
 
 function next3() {
-    document.getElementsByClassName('question_container')[0].style.display = "none";
-    document.getElementsByClassName('question_container')[1].style.display = "none";
-    document.getElementsByClassName('question_container')[2].style.display = "none";
-    document.getElementsByClassName('question_container')[3].style.display = "block";
-    document.getElementsByClassName('question_container')[4].style.display = "none";
+
+    if (document.getElementById('t3').checked ||
+        document.getElementById('af3').checked ||
+        document.getElementById('bf3').checked ||
+        document.getElementById('cf3').checked) {
+
+        var q3 = document.getElementById('t3').checked;
+
+    } else {
+        alert("Please answer Question 3 ");
+        return false;
+    }
+
+    document.getElementsByClassName('bigContainer')[0].style.display = "none";
+    document.getElementsByClassName('bigContainer')[1].style.display = "none";
+    document.getElementsByClassName('bigContainer')[2].style.display = "none";
+    document.getElementsByClassName('bigContainer')[3].style.display = "block";
+    document.getElementsByClassName('bigContainer')[4].style.display = "none";
 
     return false;
 
 }
 
 function next4() {
-    document.getElementsByClassName('question_container')[0].style.display = "none";
-    document.getElementsByClassName('question_container')[1].style.display = "none";
-    document.getElementsByClassName('question_container')[2].style.display = "none";
-    document.getElementsByClassName('question_container')[3].style.display = "none";
-    document.getElementsByClassName('question_container')[4].style.display = "block";
-    document.getElementsByClassName('submitClass')[0].style.display = "block";
+
+    if (document.getElementById('t4').checked ||
+        document.getElementById('af4').checked ||
+        document.getElementById('bf4').checked ||
+        document.getElementById('cf4').checked) {
+
+
+    } else {
+        alert("Please answer Question 4 ");
+        return false;
+    }
+
+    document.getElementsByClassName('bigContainer')[0].style.display = "none";
+    document.getElementsByClassName('bigContainer')[1].style.display = "none";
+    document.getElementsByClassName('bigContainer')[2].style.display = "none";
+    document.getElementsByClassName('bigContainer')[3].style.display = "none";
+    document.getElementsByClassName('bigContainer')[4].style.display = "block";
 
     return false;
 
